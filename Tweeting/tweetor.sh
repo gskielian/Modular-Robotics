88aa07115e08b157e5ad821821a86510d8034d02
gskielian <gregory.kielian@hackerdojo.com>
2013-10-06 22:13:25 -0700

    Update tweetor.sh

--- Tweeting/tweetor.sh
+++ Tweeting/tweetor.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 #replace youremailaddress@email.com with your email address
-# type in the message to tweet (time stamp appended because "twitter python" library doesn't allow repeat messages)
-#twitter -e youremailaddress@email.com set This is where you type your message
+#type in the message to tweet (time stamp appended because "twitter python" library doesn't allow repeat messages)
+
+#usage of "twitter" utility:
+#twitter -e youremailaddress@email.com set "This is where you type your message"
 
 twitter -e youremailaddress@email.com set "$1 `date +%s`"
